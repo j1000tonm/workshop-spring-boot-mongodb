0686412ca3d550d78f6ed69f18b7c41bbf914156
j1000tonm <j1000tonm.jmb@gmail.com>
2020-07-24 23:06:09 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/j1000tonm/workshop/mongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.j1000tonm.workshop.mongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.j1000tonm.workshop.mongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
