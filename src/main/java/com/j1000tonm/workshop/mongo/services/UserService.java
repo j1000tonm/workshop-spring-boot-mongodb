abc848de4ddf86f3e283807f0bc7eab8d20b13cd
j1000tonm <j1000tonm.jmb@gmail.com>
2020-07-25 22:35:32 -0300

    Obtendo um usuário por id

--- src/main/java/com/j1000tonm/workshop/mongo/services/UserService.java
+++ src/main/java/com/j1000tonm/workshop/mongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.j1000tonm.workshop.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.j1000tonm.workshop.mongo.domain.User;
 import com.j1000tonm.workshop.mongo.repository.UserRepository;
+import com.j1000tonm.workshop.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
