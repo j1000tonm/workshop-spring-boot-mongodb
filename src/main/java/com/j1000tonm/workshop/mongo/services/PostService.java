a64695b329828d35573746bb8848cc93074093da
j1000tonm <j1000tonm.jmb@gmail.com>
2020-07-30 23:10:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/j1000tonm/workshop/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.j1000tonm.workshop.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.j1000tonm.workshop.mongo.domain.Post;
+import com.j1000tonm.workshop.mongo.repository.PostRepository;
+import com.j1000tonm.workshop.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
